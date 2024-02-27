c4f030ae8353379f09bbeaedbd926f0841f8003b
LeinerViloria <vilorialeiner2002@gmail.com>
2024-02-26 22:15:54 -0500

    Se logra la navegacion

--- Navegacion/Views/Master.xaml.cs
+++ Navegacion/Views/Master.xaml.cs
@@ -1,3 +1,5 @@
+using Navegacion.Views.Content;
+
 namespace Navegacion.Views;
 
 public partial class Master : ContentPage
@@ -5,5 +7,18 @@ public partial class Master : ContentPage
 	public Master()
 	{
 		InitializeComponent();
+	}
+
+	private void Navigate<T>() where T : Page
+	{
+		var ViewInstance = Activator.CreateInstance<T>()!;
+
+		App.flyoutPage.Detail.Navigation.PushAsync(ViewInstance);
+		App.flyoutPage.IsPresented = false;
+	}
+
+	private void GoToPage1(object sender, EventArgs e)
+	{
+		Navigate<Chespirito>();
 	}
 }
